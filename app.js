bb205f91d74869d3dea358e85f77cec41b63389c
achuvr <m.akiyama@bipsee.co.jp>
2024-11-07 22:23:23 +0900

    fixed port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URL);
-        app.listen(PORT, console.log("server is launched."));
+        app.listen(process.env.PORT || PORT, console.log("server is launched."));
     } catch(err) {
         console.log(err);        
     }
